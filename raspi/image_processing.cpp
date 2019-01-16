4308747f9734ded293c1a7c23af7230220f92acf
MCJack123 <jackbruienne@yahoo.com>
2019-01-16 17:14:35 -0500

    Vision working

--- raspi/image_processing.cpp
+++ raspi/image_processing.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstdio>
+#include <csignal>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 #include <opencv2/opencv.hpp>
-//#include <png++/png.hpp>
 using namespace cv;
 
 #pragma pack(push, 1)
@@ -21,9 +25,36 @@ typedef struct {
 uchar pixel_threshold = 192-32;
 double brightness = 0.25;
 
+static struct termios orig_termios;  /* TERMinal I/O Structure */
+static int ttyfd = STDIN_FILENO;     /* STDIN_FILENO is 0 by default */
+/* put terminal in raw mode - see termio(7I) for modes */
+void tty_raw(void)
+   {
+    struct termios new_tio;
+	unsigned char c;
+
+	/* get the terminal settings for stdin */
+	tcgetattr(STDIN_FILENO,&orig_termios);
+
+	/* we want to keep the old setting to restore them a the end */
+	new_tio=orig_termios;
+
+	/* disable canonical mode (buffered i/o) and local echo */
+	new_tio.c_lflag &=(~ICANON & ~ECHO);
+
+	/* set the new settings immediately */
+	tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+   }
+
+void atexit(int signal) {
+    tcsetattr(ttyfd,TCSAFLUSH,&orig_termios);
+    exit(10);
+}
+
 int main() {
     // Set up capture
-    VideoCapture capture(2);
+    VideoCapture capture(0);
+    VideoWriter output("output.avi", CV_FOURCC('M', 'J', 'P', 'G'), 15, Size(640, 480*2));
     capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
     capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     capture.set(CV_CAP_PROP_CONTRAST, 1.0);
@@ -32,15 +63,36 @@ int main() {
         std::cerr << "Failed to connect to the camera.\n";
         return 1;
     }
+    signal(SIGINT, atexit);
+    signal(SIGKILL, atexit);
+    signal(SIGTERM, atexit);
+    signal(SIGHUP, atexit);
+    tty_raw();
+    fcntl (0, F_SETFL, O_NONBLOCK);
+    std::cerr << "Press + or - to change threshold\n";
     while (true) {
-        Mat frame, edges(Size(640, 480), CV_8UC1), cdst(Size(640, 480*2), CV_8UC3);
+        Mat frame, edges(Size(640, 480), CV_8UC1), denoised, cdst(Size(640, 480*2), CV_8UC3);
         capture >> frame;
         if(frame.empty()){
             std::cerr << "Failed to capture an image.\n";
             return 2;
         }
-        // Create PNG
-        //png::image<png::rgb_pixel> img(640, 480*2);
+        char c = 0;
+        ::read(0, &c, 1);
+        if (c == '=' || c == '+') std::cerr << "Threshold now " << (int)++pixel_threshold << "\n";
+        else if (c == '-' || c == '_') std::cerr << "Threshold now " << (int)--pixel_threshold << "\n";
+        // Get min/max values
+        uchar min = 255, max = 0;
+        for (int x = 0; x < frame.rows; x++) {
+            for (int y = 0; y < frame.cols; y++) {
+                pixel_t pix = mpx(frame.at<Vec3b>(x, y).val);
+                uchar avg = (pix.r + pix.g + pix.b) / 3;
+                if (avg > max) max = avg;
+                if (avg < min) min = avg;
+            }
+        }
+        //std::cerr << "min = " << (int)min << ", max = " << (int)max << "\n";
+        uchar thresh = ((max - min) * (pixel_threshold / 256.0)) + min;
         // Convert to B/W image
         std::vector<bool> lastRow(640, 0);
         for (int x = 0; x < frame.rows; x++) {
@@ -48,38 +100,20 @@ int main() {
             for (int y = 0; y < frame.cols; y++) {
                 pixel_t pix = mpx(frame.at<Vec3b>(x, y).val);
                 uchar avg = (pix.r + pix.g + pix.b) / 3;
-                /*bool v = (avg >= pixel_threshold) != lastValue || 
-                        (avg >= pixel_threshold) != lastRow[y] || 
-                        (avg >= pixel_threshold && y == frame.cols - 1) ||
-                        (avg >= pixel_threshold && x == frame.rows - 1);*/
-                bool v = avg >= pixel_threshold;
+                bool v = avg >= thresh;
                 edges.at<uchar>(x, y) = v * 255;
-                //img.set_pixel(y, x, png::rgb_pixel(v * 255, v * 255, v * 255));
-                //img.set_pixel(y, x + 480, png::rgb_pixel(pix.r, pix.g, pix.b));
-                cdst.at<Vec3b>(x + 480, y) = Vec3b(v * 255, v * 255, v * 255);
-                cdst.at<Vec3b>(x, y) = Vec3b(pix.b, pix.g, pix.r);
-                lastValue = avg >= pixel_threshold;
+                cdst.at<Vec3b>(x, y) = Vec3b(v * 255, v * 255, v * 255);
+                cdst.at<Vec3b>(x + 480, y) = Vec3b(pix.b, pix.g, pix.r);
+                lastValue = avg >= thresh;
                 lastRow[y] = lastValue;
             }
         }
-        // Detect lines
-        /*std::vector<Vec4i> lines;
-        HoughLinesP(edges, lines, 1, CV_PI / 180, 50, 50, 10);
-        for (Vec4i line : lines) {
-            point_t a, b, la = {line[0], line[1]}, lb = {line[2], line[3]};
-            a.x = 0;
-            a.y = sqrt((lb.x - la.x)^2 + (lb.y - la.y)^2);
-            b.x = lb.x - la.x;
-            b.y = lb.y - la.y;
-            double angle = (atan2(a.y, a.x) - atan2(b.y, b.x)) * (180/M_PI);
-            cv::line(cdst, Point(line[0], line[1] + 960), Point(line[2], line[3] + 960), Scalar(255, 255, 255), 3, 2);
-            std::cout << "First point at (" << line[0] << ", " << line[1] << "), last point at (" << line[2] << ", " << line[3] << "), angle = " << angle << "\n";
-        }*/
         // extract contours
         std::vector<std::vector<cv::Point> > contours;
         cv::findContours(edges, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-        if (contours.size() > 1) continue;
-        for(int i=0; i<contours.size(); ++i)
+        if (contours.size() < 1) continue;
+        //for(int i=0; i<1; ++i)
+        int i = 0;
         {
             // fit bounding rectangle around contour
             cv::RotatedRect rotatedRect = cv::minAreaRect(contours[i]);
@@ -101,7 +135,7 @@ int main() {
 
 
             angle = 180.0f/CV_PI * acos((reference.x*usedEdge.x + reference.y*usedEdge.y) / (cv::norm(reference) *cv::norm(usedEdge)));
-
+            if (angle == 90.0 || angle == -90.0 || angle == 0.0) continue;
             // read center of rotated rect
             cv::Point2f center = rotatedRect.center; // center
 
@@ -110,14 +144,11 @@ int main() {
                 cv::line(cdst, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,255,0));
 
             // draw center and print text
-            if (!isnan(angle - 90.0)) std::cout << angle - 90.0 << "\n";
-            imshow("Rect", cdst);
-            //std::stringstream ss;   ss << angle; // convert float to string
-            //cv::circle(cdst, center, 5, cv::Scalar(0,255,0)); // draw center
-            //cv::putText(cdst, ss.str(), center + cv::Point2f(-25,25), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(255,0,255)); // print angle
+            if (!isnan(angle - 90.0)) std::cerr << angle - 90.0 << "\n";
+            std::vector<uchar> buf;
+            imencode(".jpg", cdst, buf);
+            std::cout << std::string((char*)&buf[0], buf.size());
         }
     }
-    //img.write("detected_image.png");
-    //imwrite("lines.png", cdst);
     return 0;
 }
