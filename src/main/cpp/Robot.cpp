0496c78e3d981b6dff0fe0cfd63624bd3774c44e
nstringham <nstringham@wccnet.edu>
2019-01-22 16:35:44 -0500

    Fixing Elliot (oh wait, we can't fix him)

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -139,7 +139,7 @@ public:
 		}
 
 
-		leftLeader.Set(ControlMode::PercentOutput, lefttarget);
+		leftLeader.Set(ControlMode::PercentOutput, leftTarget);
 		//leftFollower.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, leftLeader.GetMotorOutputPercent());
 		//leftFollower.Set(ControlMode::PercentOutput, leftTarget);
 
