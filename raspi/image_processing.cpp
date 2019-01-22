5af94954687eab617754925665c619378acdc597
MCJack123 <jackbruienne@yahoo.com>
2019-01-22 16:11:07 -0500

    Added vision timing

--- raspi/image_processing.cpp
+++ raspi/image_processing.cpp
@@ -9,9 +9,234 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <wiringSerial.h>
+//#include <wiringSerial.h>
+/*
+ * wiringSerial.c:
+ *	Handle a serial port
+ ***********************************************************************
+ * This file is part of wiringPi:
+ *	https://projects.drogon.net/raspberry-pi/wiringpi/
+ *
+ *    wiringPi is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    wiringPi is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with wiringPi.  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*
+ * serialOpen:
+ *	Open and initialise the serial port, setting all the right
+ *	port parameters - or as many as are required - hopefully!
+ *********************************************************************************
+ */
+
+int serialOpen (const char *device, const int baud)
+{
+  struct termios options ;
+  speed_t myBaud ;
+  int     status, fd ;
+
+  switch (baud)
+  {
+    case      50:	myBaud =      B50 ; break ;
+    case      75:	myBaud =      B75 ; break ;
+    case     110:	myBaud =     B110 ; break ;
+    case     134:	myBaud =     B134 ; break ;
+    case     150:	myBaud =     B150 ; break ;
+    case     200:	myBaud =     B200 ; break ;
+    case     300:	myBaud =     B300 ; break ;
+    case     600:	myBaud =     B600 ; break ;
+    case    1200:	myBaud =    B1200 ; break ;
+    case    1800:	myBaud =    B1800 ; break ;
+    case    2400:	myBaud =    B2400 ; break ;
+    case    4800:	myBaud =    B4800 ; break ;
+    case    9600:	myBaud =    B9600 ; break ;
+    case   19200:	myBaud =   B19200 ; break ;
+    case   38400:	myBaud =   B38400 ; break ;
+    case   57600:	myBaud =   B57600 ; break ;
+    case  115200:	myBaud =  B115200 ; break ;
+    case  230400:	myBaud =  B230400 ; break ;
+    case  460800:	myBaud =  B460800 ; break ;
+    case  500000:	myBaud =  B500000 ; break ;
+    case  576000:	myBaud =  B576000 ; break ;
+    case  921600:	myBaud =  B921600 ; break ;
+    case 1000000:	myBaud = B1000000 ; break ;
+    case 1152000:	myBaud = B1152000 ; break ;
+    case 1500000:	myBaud = B1500000 ; break ;
+    case 2000000:	myBaud = B2000000 ; break ;
+    case 2500000:	myBaud = B2500000 ; break ;
+    case 3000000:	myBaud = B3000000 ; break ;
+    case 3500000:	myBaud = B3500000 ; break ;
+    case 4000000:	myBaud = B4000000 ; break ;
+
+    default:
+      return -2 ;
+  }
+
+  if ((fd = open (device, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK)) == -1)
+    return -1 ;
+
+  fcntl (fd, F_SETFL, O_RDWR) ;
+
+// Get and modify current options:
+
+  tcgetattr (fd, &options) ;
+
+    cfmakeraw   (&options) ;
+    cfsetispeed (&options, myBaud) ;
+    cfsetospeed (&options, myBaud) ;
+
+    options.c_cflag |= (CLOCAL | CREAD) ;
+    options.c_cflag &= ~PARENB ;
+    options.c_cflag &= ~CSTOPB ;
+    options.c_cflag &= ~CSIZE ;
+    options.c_cflag |= CS8 ;
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG) ;
+    options.c_oflag &= ~OPOST ;
+
+    options.c_cc [VMIN]  =   0 ;
+    options.c_cc [VTIME] = 100 ;	// Ten seconds (100 deciseconds)
+
+  tcsetattr (fd, TCSANOW, &options) ;
+
+  ioctl (fd, TIOCMGET, &status);
+
+  status |= TIOCM_DTR ;
+  status |= TIOCM_RTS ;
+
+  ioctl (fd, TIOCMSET, &status);
+
+  usleep (10000) ;	// 10mS
+
+  return fd ;
+}
+
+
+/*
+ * serialFlush:
+ *	Flush the serial buffers (both tx & rx)
+ *********************************************************************************
+ */
+
+void serialFlush (const int fd)
+{
+  tcflush (fd, TCIOFLUSH) ;
+}
+
+
+/*
+ * serialClose:
+ *	Release the serial port
+ *********************************************************************************
+ */
+
+void serialClose (const int fd)
+{
+  close (fd) ;
+}
+
+
+/*
+ * serialPutchar:
+ *	Send a single character to the serial port
+ *********************************************************************************
+ */
+
+void serialPutchar (const int fd, const unsigned char c)
+{
+  write (fd, &c, 1) ;
+}
+
+
+/*
+ * serialPuts:
+ *	Send a string to the serial port
+ *********************************************************************************
+ */
+
+void serialPuts (const int fd, const char *s)
+{
+  write (fd, s, strlen (s)) ;
+}
+
+/*
+ * serialPrintf:
+ *	Printf over Serial
+ *********************************************************************************
+ */
+
+void serialPrintf (const int fd, const char *message, ...)
+{
+  va_list argp ;
+  char buffer [1024] ;
+
+  va_start (argp, message) ;
+    vsnprintf (buffer, 1023, message, argp) ;
+  va_end (argp) ;
+
+  serialPuts (fd, buffer) ;
+}
+
+
+/*
+ * serialDataAvail:
+ *	Return the number of bytes of data avalable to be read in the serial port
+ *********************************************************************************
+ */
+
+int serialDataAvail (const int fd)
+{
+  int result ;
+
+  if (ioctl (fd, FIONREAD, &result) == -1)
+    return -1 ;
+
+  return result ;
+}
+
+
+/*
+ * serialGetchar:
+ *	Get a single character from the serial device.
+ *	Note: Zero is a valid character and this function will time-out after
+ *	10 seconds.
+ *********************************************************************************
+ */
+
+int serialGetchar (const int fd)
+{
+  uint8_t x ;
+
+  if (read (fd, &x, 1) != 1)
+    return -1 ;
+
+  return ((int)x) & 0xFF ;
+}
+#include <chrono>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+using namespace std::chrono;
 
 #pragma pack(push, 1)
 typedef struct {
@@ -47,12 +272,15 @@ info frame:
 | 2 | Checksum |
  */
 bool verbose = false;
+steady_clock::time_point t;
 
-void outputImage(Mat cdst) {
+void outputImage(Mat cdst, bool success) {
     if (verbose) {
         std::vector<uchar> buf;
         imencode(".jpg", cdst, buf);
         std::cout << std::string((char*)&buf[0], buf.size());
+        if (success) std::cerr << "Got frame in " << (duration_cast<milliseconds>(steady_clock::now() - t)).count() << "ms\n";
+        else std::cerr << "Failed to get frame\n";
     }
 }
 
@@ -84,9 +312,16 @@ int main(int argc, const char * argv[]) {
         ::write(serout, frames, 16);
     }
     while (true) {
+        t = steady_clock::now();
         uint32_t frames[4];
         frames[0] = VISION_HEADER;
-        Mat frame, edges(Size(640, 480), CV_8UC1), denoised, cdst(Size(640, 480*2), CV_8UC3);
+        Mat frame, cdst(Size(640, 480*2), CV_8UC3);
+        #ifdef CUDA
+        cv::gpu::GpuMat edges(Size(640, 480), CV_8UC1), denoised;
+        gpu::GpuMat edges, denoised;
+        #else
+        Mat edges(Size(640, 480), CV_8UC1), denoised;
+        #endif
         capture >> frame;
         if(frame.empty()) {
             std::cerr << "Failed to capture an image.\n";
@@ -112,15 +347,8 @@ int main(int argc, const char * argv[]) {
             frames[0] = VISION_HEADER;
         }
         // Get min/max values
-        uchar min = 255, max = 0;
-        for (int x = 0; x < frame.rows; x++) {
-            for (int y = 0; y < frame.cols; y++) {
-                pixel_t pix = mpx(frame.at<Vec3b>(x, y).val);
-                uchar avg = (pix.r + pix.g + pix.b) / 3;
-                if (avg > max) max = avg;
-                if (avg < min) min = avg;
-            }
-        }
+        double min = 0, max = 0;
+        
         //std::cerr << "min = " << (int)min << ", max = " << (int)max << "\n";
         uchar thresh = ((max - min) * (pixel_threshold / 256.0)) + min;
         // Convert to B/W image
@@ -141,7 +369,7 @@ int main(int argc, const char * argv[]) {
         // extract contours
         std::vector<std::vector<cv::Point> > contours;
         cv::findContours(edges, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-        if (contours.size() < 1) {outputImage(cdst); continue;}
+        if (contours.size() < 1) {outputImage(cdst, false); continue;}
         //for(int i=0; i<1; ++i)
         int i = 0;
         // fit bounding rectangle around contour
@@ -172,7 +400,7 @@ int main(int argc, const char * argv[]) {
         for(unsigned int j=0; j<4; ++j)
             cv::line(cdst, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,255,0));
 
-        if (isnan(angle)) {outputImage(cdst); continue;}
+        if (isnan(angle)) {outputImage(cdst, false); continue;}
         frames[1] = *(uint32_t*)(&angle);
         
         // Calculate offset
@@ -185,7 +413,7 @@ int main(int argc, const char * argv[]) {
         float inchDistance = multiplier * 2.0;
         frames[2] = *(uint32_t*)(&cmDistance);
         //std::cerr << cmDistance << "\n";
-        if (verbose) cv::line(cdst, Point2f(320.0, 240.0), center, cv::Scalar(0, 255, 0));
+        if (verbose) cv::line(cdst, Point2f(320.0, 240.0), Point2f(center.x, 240.0), cv::Scalar(0, 255, 0));
 
         // TODO: get distance data
         uint16_t * frames16 = (uint16_t*)frames;
@@ -200,7 +428,7 @@ int main(int argc, const char * argv[]) {
         ::write(serout, frames, 16);
 
         // output final image
-        outputImage(cdst);
+        outputImage(cdst, true);
     }
     return 0;
 }
