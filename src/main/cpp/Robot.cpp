8529bf694874a00ad5144b6ca04769510e7491d8
nstringham <nstringham@wccnet.edu>
2019-02-07 14:44:36 -0500

    Cleaned up the Mess and orginized and commented stuff

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -37,7 +37,6 @@ private:
 	const string kAutoNameDefault = "Default";
 	const string kAutoNameCustom = "My Auto";
 	string m_autoSelected;
-
 	
 
 	XboxController xboxController{0};
@@ -54,16 +53,7 @@ private:
 	bool rightShoulder;//not used yet
 	bool leftShoulder;
 
-	//Robot propeties
-	const float DIST_FROM_RIGHT_WEEL_TO_LEFT_WEEL = 2.0f;  //TODO set this to correct value
-	const float DIST_CENTER_AXEL_TO_CAM = 1.0f;  //TODO set this to correct value
-	const float DIST_CENTER_AXEL_TO_HATCH_PANEL = 1.0f;  //TODO set this to correct value
-	const float HORIZONTAL_FOV = 60.0f;  //TODO set this to correct value
-	const float DIST_TO_MANTAIN_CAM_ANGLE = 1.0f;  //TODO set this to correct value
-	const float DIST_TO_MANTAIN_FINAL_ANGLE = 0.5f;  //TODO set this to correct value
-	const float ANGLE_MAX_ERROR = 5.0f;  //TODO set this to correct value
-	
-	//whether input comes from joystick or auton
+	//Auton
 	bool isAuton = false;
 	bool isOffHab = false;
 	
@@ -92,202 +82,16 @@ public:
 
 	SerialPort serial_port = SerialPort(9600, SerialPort::Port::kUSB);
 
+	//this gets called once at the beggining of the match
 	void RobotInit() {
+		SetupMoters();
 		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
 		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
 		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 		serial_port.SetTimeout(.05);
 	}
 
-	/*
-	 * This autonomous (along with the chooser code above) shows how to
-	 * select between different autonomous modes using the dashboard. The
-	 * sendable chooser code works with the Java SmartDashboard. If you
-	 * prefer the LabVIEW Dashboard, remove all of the chooser code and
-	 * uncomment the GetString line to get the auto name from the text box
-	 * below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to
-	 * the if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as
-	 * well.
-	 */
-	void AutonomousInit() override {
-		m_autoSelected = m_chooser.GetSelected();
-
-		// m_autoSelected = SmartDashboard::GetString("Auto Selector",
-		//		 kAutoNameDefault);
-		cout << "Auto selected: " << m_autoSelected << endl;
-
-		if (m_autoSelected == kAutoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void AutonomousPeriodic() {
-		Drive();
-		if (m_autoSelected == kAutoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void TeleopInit() {
-		SetupMoters();
-		ResetGyro();
-		ResetEncoders();
-
-		cout << rightLeader.GetSelectedSensorPosition() << endl;	
-		//cout << works << endl;
-	}
-
-	void ResetGyro() {
-		pGyro->ZeroYaw();
-		while(!(pGyro->GetYaw() < 0.01 && pGyro->GetYaw() > -.01)) {}
-	}
-
-	void ResetEncoders() {
-		leftLeader.SetSelectedSensorPosition(0, Constant::pidChannel, 50);
-		rightLeader.SetSelectedSensorPosition(0, Constant::pidChannel, 50);
-		cout << "reseting encoders\n"; 
-	}
-
-	bool GetOffHab() {
-		if(DriveDistance(36,1)) {
-			ResetEncoders();
-			isOffHab = true; 
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-
-	void TeleopPeriodic() {
-		Drive();
-		//cout << DriveDistance(6 * 3.14, 1) << endl;
-		//DriveDistance(1, 1);
-		//cout << gyro->GetAngle() << endl;
-		
-		//cout << rightLeader.GetSelectedSensorPosition() << endl;
-		//cout << leftLeader.GetSelectedSensorPosition() << endl;
-	}
-
-	void Drive() {
-		UpdateControllerInputs();
-		UpdateRaspiInput();
-		isAuton = leftShoulder;
-
-		if(isAuton) {
-			if(!isOffHab) {
-				GetOffHab();
-			}		
-		}
-		else {
-			leftTarget = leftjoyY;
-			rightTarget = rightjoyY;
-			
-			//Set the motors to the motor targets
-			leftLeader.Set(ControlMode::PercentOutput, leftTarget);
-			rightLeader.Set(ControlMode::PercentOutput, -rightTarget);
-
-			
-		}
-
-	}
-
-	bool ApproachLine(float targetDistance) {
-		//returns true when done
-		CalculateAutonInstructions();
-	}
-
-	bool CalculateAutonInstructions() {
-		//returns true if AutonInstructions have bean generated successfully
-
-		float outPut [100] = {}; //autonInstructions will be set to this if successful
-
-		float x;
-		float y;
-		float targetAngle = (float)atan(y / x) * 180 / PI;
-		if(fabs(targetAngle - frame.angle) < ANGLE_MAX_ERROR) {
-			outPut[1] = (float)atan(y / x) * 180 / PI; //TODO fix this its not exact
-			outPut[0] = (float)sqrt( x*x + y*y ); //TODO fix this its not exact
-			for(int i = 0; i < 100; i++){
-				autonInstructions[i] = outPut[i];
-			}
-			return true;
-		}
-		else{
-			outPut[0] = DIST_TO_MANTAIN_FINAL_ANGLE;
-			float alinedAtX;
-			float alinedAtY;
-			if(fabs(targetAngle - frame.angle) < (HORIZONTAL_FOV / 2)){
-			}
-			else{
-				outPut[2] = DIST_TO_MANTAIN_CAM_ANGLE;
-				outPut[1] = HORIZONTAL_FOV / 2;
-			}
-		}
-		return false;
-	}
-
-	bool AutonPositionDeadband(double value, int target) {
-		if (fabs(target - value) < Constant::autonPositionDeadbandVal) {
-			return true; 
-		} else {
-			return false;
-		}
-	}
-
-
-	bool DriveDistance(double inches, float speed) {
-		//speed is a percentage from 0.0 to 1.0
-
-		// inches / circumference = number of rotations
-		// * pulsesPerRotationQuad = number of pulses in one rotation
-		// targetEncPos = position encoder should read
-		//int targetEncPos = (inches / Constant::circumference) * Constant::pulsesPerRotationQuad;
-		
-		int targetEncPos = (inches / Constant::circumference) * Constant::pulsesPerRotationQuad;
-
-		if (AutonPositionDeadband(leftLeader.GetSelectedSensorPosition(Constant::pidChannel), targetEncPos)) {
-			leftLeader.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-			rightLeader.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
-			return true;
-		}
-		//cout << "rightLeader.GetSelectedSensorPosition(): " << rightLeader.GetSelectedSensorPosition() << endl;
-
-		if(speed > 1)
-		speed = 1;
-		if(speed < 0)
-		speed = 0;
-		leftLeader.ConfigMotionCruiseVelocity(speed * Constant::leftMotionVel);
-		rightLeader.ConfigMotionCruiseVelocity(speed * Constant::rightMotionVel);
-		leftLeader.Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, targetEncPos);
-		rightLeader.Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, targetEncPos);
-
-		return false;
-	}
-
-
-	bool TurnDegrees(double degrees) {
-		cout << pGyro->GetYaw() << endl;
-
-		if(pidAngle.GetSetpoint() != degrees) {
-			pidAngle.SetSetpoint(degrees);
-		}
-
-		if(pidAngle.IsEnabled() && pidAngle.OnTarget()) {
-			pidAngle.Disable();
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+	//configures the motors should be called at begining of match
 	void SetupMoters() {
 		//Right motor setup
 		leftLeader.ClearStickyFaults(0);
@@ -352,7 +156,113 @@ public:
 
 	}
 
-	//This Method will get inputs from raspi
+	/*
+	 * This autonomous (along with the chooser code above) shows how to
+	 * select between different autonomous modes using the dashboard. The
+	 * sendable chooser code works with the Java SmartDashboard. If you
+	 * prefer the LabVIEW Dashboard, remove all of the chooser code and
+	 * uncomment the GetString line to get the auto name from the text box
+	 * below the Gyro.
+	 *
+	 * You can add additional auto modes by adding additional comparisons to
+	 * the if-else structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as
+	 * well.
+	 */
+
+	//this does nothing in 2019
+	void AutonomousInit() override {
+		m_autoSelected = m_chooser.GetSelected();
+
+		// m_autoSelected = SmartDashboard::GetString("Auto Selector",
+		//		 kAutoNameDefault);
+		cout << "Auto selected: " << m_autoSelected << endl;
+
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	//this code does nothing in 2019
+	void AutonomousPeriodic() {
+		Drive();
+		if (m_autoSelected == kAutoNameCustom) {
+			// Custom Auto goes here
+		} else {
+			// Default Auto goes here
+		}
+	}
+
+	//is called once at the begining of the mathc in 2019
+	void TeleopInit() {
+		ResetGyro();
+		ResetEncoders();
+	}
+
+	//this code gets call once every tick or about 50 times per second
+	void TeleopPeriodic() {
+		Drive();
+		//cout << DriveDistance(6 * 3.14, 1) << endl;
+		//DriveDistance(1, 1);
+		//cout << gyro->GetAngle() << endl;
+		
+		//cout << rightLeader.GetSelectedSensorPosition() << endl;
+		//cout << leftLeader.GetSelectedSensorPosition() << endl;
+	}
+
+	//this gets called every tick and contans all the drivetrain related code
+	void Drive() {
+		UpdateControllerInputs();
+		UpdateRaspiInput();
+		isAuton = leftShoulder;
+
+		if(isAuton) {
+			if(!isOffHab) {
+				GetOffHab();
+			}		
+		}
+		else {
+			leftTarget = leftjoyY;
+			rightTarget = rightjoyY;
+			
+			//Set the motors to the motor targets
+			leftLeader.Set(ControlMode::PercentOutput, leftTarget);
+			rightLeader.Set(ControlMode::PercentOutput, -rightTarget);
+
+			
+		}
+
+	}
+
+	// polls the xbox for input
+	void UpdateControllerInputs() {
+		//Tank drive both stick
+		leftjoyY = -xboxController.GetY(frc::GenericHID::JoystickHand::kLeftHand);
+		rightjoyY = xboxController.GetY(frc::GenericHID::JoystickHand::kRightHand);
+		
+		//Auton overide
+		leftShoulder = xboxController.GetBumper(frc::GenericHID::JoystickHand::kLeftHand);
+
+
+#ifdef FINEMOTIONCONTROL
+		//If right trigger pressed
+		if((xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kRightHand) >0.1)  && !(xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand))) {
+			//Kinda slow down
+			leftjoyY = leftjoyY / 1.5;
+			rightjoyY = rightjoyY / 1.5;
+		}
+		//If both triggers pressed
+		if((xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand)) >0.1 && (xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kRightHand) >0.1)) {
+			//Really slow down
+			leftjoyY = leftjoyY / 3;
+			rightjoyY = rightjoyY / 3;
+		}
+#endif
+	}
+
+	//gets inputs from raspi
 	void UpdateRaspiInput() {
 		frame = getFrame(serial_port);
 		if (frame.error) {
@@ -368,6 +278,146 @@ public:
 		last_frame_time = time_clock.now();
 	}
 
+	//call this every tick to get off the hab
+	bool GetOffHab() {
+		//returns true when done
+		if(DriveDistance(36,1)) {
+			ResetEncoders();
+			isOffHab = true; 
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//drives "inches" inches at "speed" the cruise velocity
+	bool DriveDistance(double inches, float speed) {
+		//speed is a percentage from 0.0 to 1.0
+
+		// inches / circumference = number of rotations
+		// * pulsesPerRotationQuad = number of pulses in one rotation
+		// targetEncPos = position encoder should read
+		//int targetEncPos = (inches / Constant::circumference) * Constant::pulsesPerRotationQuad;
+		
+		int targetEncPos = (inches / Constant::circumference) * Constant::pulsesPerRotationQuad;
+
+		if (AutonPositionDeadband(leftLeader.GetSelectedSensorPosition(Constant::pidChannel), targetEncPos)) {
+			leftLeader.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+			rightLeader.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0);
+			return true;
+		}
+		//cout << "rightLeader.GetSelectedSensorPosition(): " << rightLeader.GetSelectedSensorPosition() << endl;
+
+		if(speed > 1)
+		speed = 1;
+		if(speed < 0)
+		speed = 0;
+		leftLeader.ConfigMotionCruiseVelocity(speed * Constant::leftMotionVel);
+		rightLeader.ConfigMotionCruiseVelocity(speed * Constant::rightMotionVel);
+		leftLeader.Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, targetEncPos);
+		rightLeader.Set(ctre::phoenix::motorcontrol::ControlMode::MotionMagic, targetEncPos);
+
+		return false;
+	}
+
+	//checks margen of error for DriveDistance()
+	bool AutonPositionDeadband(double value, int target) {
+		//returns true if "value" is within the margen of error of "target"
+		if (fabs(target - value) < Constant::autonPositionDeadbandVal) {
+			return true; 
+		} else {
+			return false;
+		}
+	}
+
+	//sets encoder position to zero
+	void ResetEncoders() {
+		//this will reset the progress of DriveDistance()
+		leftLeader.SetSelectedSensorPosition(0, Constant::pidChannel, 50);  // 50 is the number of ms before it times out
+		rightLeader.SetSelectedSensorPosition(0, Constant::pidChannel, 50);  // 50 is the number of ms before it times out
+	}
+
+	//call this once per tick to dive "targetDistance" inches away from the end of the line
+	//TODO
+	bool ApproachLine(float targetDistance) {
+		//returns true when done
+		CalculateAutonInstructions();
+	}
+
+	//call this to update autonInstructions[] make sure to call UpdateRaspiInput() first
+	//TODO
+	bool CalculateAutonInstructions() {
+		//returns true if AutonInstructions have bean generated successfully
+
+		float outPut [100] = {}; //autonInstructions will be set to this if successful
+
+		float x;
+		float y;
+		float targetAngle = (float)atan(y / x) * 180 / PI;
+		if(fabs(targetAngle - frame.angle) < ANGLE_MAX_ERROR) {
+			outPut[1] = (float)atan(y / x) * 180 / PI; //TODO fix this its not exact
+			outPut[0] = (float)sqrt( x*x + y*y ); //TODO fix this its not exact
+			for(int i = 0; i < 100; i++){
+				autonInstructions[i] = outPut[i];
+			}
+			return true;
+		}
+		else{
+			outPut[0] = DIST_TO_MANTAIN_FINAL_ANGLE;
+			float alinedAtX;
+			float alinedAtY;
+			if(fabs(targetAngle - frame.angle) < (HORIZONTAL_FOV / 2)){
+			}
+			else{
+				outPut[2] = DIST_TO_MANTAIN_CAM_ANGLE;
+				outPut[1] = HORIZONTAL_FOV / 2;
+			}
+		}
+		return false;
+	}
+
+	//TODO make a comment that explains what this does
+	bool TurnDegrees(double degrees) {
+		cout << pGyro->GetYaw() << endl;
+
+		if(pidAngle.GetSetpoint() != degrees) {
+			pidAngle.SetSetpoint(degrees);
+		}
+
+		if(pidAngle.IsEnabled() && pidAngle.OnTarget()) {
+			pidAngle.Disable();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	//TODO add a coment here explaning what ResetGyro() does
+	void ResetGyro() {
+		pGyro->ZeroYaw();
+		while(!(pGyro->GetYaw() < 0.01 && pGyro->GetYaw() > -.01)) {}
+	}
+
+	//call every tick to climb hab
+	void Climb() {
+		//returns true when done
+	}
+
+	//sets "pistonID" to extended if true and retracted if false
+	//TODO
+	void SetPistonExtended(int pistonID, bool value){
+
+	}
+
+	//checks whether the piston is extended
+	//TODO
+	bool GetPistonExtended(int pistonID){
+		//returns true if "pistonID" is extended false if retracted
+		
+	}
+
+	//checks if auton can ApproachLine()
+	//TODO
 	bool IsLineApproachable() {
 		if (chrono::duration_cast<chrono::milliseconds>(time_clock.now() - last_frame_time).count() > 35) // old frame is outdated
 			UpdateRaspiInput();
@@ -375,6 +425,8 @@ public:
 		        frame.angle != 0 && frame.line_offset != 0 && !frame.error); // check for problems
 	}
 
+	//this is a duplicate of ApproachLine()
+	//TODO is this better than ApproachLine()?
 	uint8_t dock_state = 0;
 	int DockRobot() {
 		if (IsLineApproachable()) {
@@ -414,70 +466,46 @@ public:
 		}
 	}
 
-	void UpdateControllerInputs() {
-		//Tank drive both stick
-		leftjoyY = -xboxController.GetY(frc::GenericHID::JoystickHand::kLeftHand);
-		rightjoyY = xboxController.GetY(frc::GenericHID::JoystickHand::kRightHand);
-		
-		//Auton overide
-		leftShoulder = xboxController.GetBumper(frc::GenericHID::JoystickHand::kLeftHand);
-
-		
-
-#ifdef FINEMOTIONCONTROL
-		//If right trigger pressed
-		if((xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kRightHand) >0.1)  && !(xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand))) {
-			//Kinda slow down
-			leftjoyY = leftjoyY / 1.5;
-			rightjoyY = rightjoyY / 1.5;
-		}
-		//If both triggers pressed
-		if((xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand)) >0.1 && (xboxController.GetTriggerAxis(frc::GenericHID::JoystickHand::kRightHand) >0.1)) {
-			//Really slow down
-			leftjoyY = leftjoyY / 3;
-			rightjoyY = rightjoyY / 3;
-		}
-#endif
-	}
-
-	void SetArmAngle(float angle){
+	//call every tick to take a hatch pannel from the brushes
+	//TODO
+	bool RetrieveHatch(){
+		//returns true when done
 
 	}
 
-	void SetPistonExtended(int pistonID){
+	//call every tick to place the hatch pannel on a rocket or cargoship
+	//TODO
+	bool PlaceHatch(){
+		//returns true when done
+
+		//SetArmAngle();
+		//DriveDistance();
+		//EjectHatch();
+	}
+
+	//call every tick to set the arm angle to "angle"
+	//TODO
+	bool SetArmAngle(float angle){
+		//returns true when done
+	}
+
+	//fires pistons to remove hatch from robot velcrow
+	//TODO
+	void EjectHatch(){
 
 	}
 
-	// bool SetPistonExtended(int pistonID){
-	// 	return false;
-	// }
-
-	void PlaceHatch(){
+	// call every tick to put the orange ball in a cargoship/rocket
+	bool PlaceCargo(){
+		//returns true when done
 
 	}
 
-	void PlaceCargo(){
-
-	}
-
-	void RetrieveHatch(){
-
-	}
-
+	//spins weels to shot out cargo
+	//TODO
 	void EjectCargo(){
 
 	}
-
-	//stuff we need to do
-	//setArmAngle(float angle)
-	//setPistonExtended(int pistonID)
-	//getPistonExtended(int pistonID)
-	//placeHatch()
-	//placeCargo(float rocket or ship angle)
-	//retrieveHatch()
-	//ejectCargo()
-
-
 };
 
 START_ROBOT_CLASS(Robot);
